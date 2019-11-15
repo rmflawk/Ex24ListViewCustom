f4d3de1e4680de3d83e13c3c9d874a2377084e71
rmflawk <ant921111@naver.com>
2019-11-15 13:51:37 +0900

    1

---
+++ app/src/main/java/com/youngstudio/ex24listviewcustom/Member.java
@@ -0,0 +1,19 @@
+package com.youngstudio.ex24listviewcustom;
+
+
+public class Member {
+
+    int imgID;     //R.drawable.korea
+    String name;   //"전현무"
+    String nation; //"대한민국"
+
+    //생성(new)할때 자동으로 실행되는 메소드
+    //생성자메소드
+    public Member(int imgID, String name, String nation){
+        //멤버변수에 값 대입
+        this.imgID= imgID;
+        this.name= name;
+        this.nation= nation;
+    }
+
+}
